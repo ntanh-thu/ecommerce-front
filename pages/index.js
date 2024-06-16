422f2b6486c387e4060e6a53621853b3d92dd782
Nguyễn Thị Anh Thư <94220054+ntanh-thu@users.noreply.github.com>
2024-06-16 18:35:46 +0700

    ecommerce-front: featured product

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import { mongooseConnect } from "@/lib/mongooes";
+import { Product } from "@/models/Products";
 
-export default function HomePage() {
+export default function HomePage({ product }) {
+  console.log(product);
   return (
     <div>
       <Header />
-      <Featured />
+      <Featured product={product} />
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const featuredProductId = "666eb08db283e4dee46e0b11";
+  await mongooseConnect();
+  const product = await Product.findById(featuredProductId);
+  return {
+    props: { product: JSON.parse(JSON.stringify(product)) },
+  };
 }
