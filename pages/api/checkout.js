8213948c18ada29b77e40bde49367503e883e3da
Nguyễn Thị Anh Thư <94220054+ntanh-thu@users.noreply.github.com>
2024-06-19 23:46:13 +0700

    stripe checkout

---
+++ pages/api/checkout.js
@@ -0,0 +1,56 @@
+import { mongooseConnect } from "@/lib/mongooes";
+import { Order } from "@/models/Order";
+import { Product } from "@/models/Products";
+const stripe = require("stripe")(process.env.STRIPE_KEY_SECRET);
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.json("should be a POST request");
+  }
+  await mongooseConnect();
+  const { name, email, city, postalCode, streetAddress, country, cartProducts } = req.body;
+  const productIds = cartProducts;
+  const uniqueIds = [...new Set(productIds)];
+  const productInfos = await Product.find({ _id: uniqueIds });
+  let line_items = [];
+  console.log(uniqueIds);
+
+  for (const productId of uniqueIds) {
+    const productinfo = productInfos.find((p) => p._id.toString() === productId);
+    const quantity = productIds.filter((id) => id === productId).length || 0;
+    if (quantity > 0 && productinfo) {
+      line_items.push({
+        quantity,
+        price_data: {
+          currency: "USD",
+          product_data: { name: productinfo.title },
+          unit_amount: parseInt(productinfo.price) * 100,
+        },
+      });
+    }
+  }
+  const orderDoc = await Order.create({
+    line_items,
+    name,
+    email,
+    city,
+    postalCode,
+    streetAddress,
+    country,
+    paid: false,
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: "payment",
+    customer_email: email,
+    success_url: process.env.PUBLIC_URL + "/cart?success=1",
+    cancel_url: process.env.PUBLIC_URL + "/cart?canceled=1",
+    metadata: {
+      orderId: orderDoc._id.toString(),
+    },
+  });
+  res.json({
+    url: session.url,
+  });
+}
--- pages/cart.js
+++ pages/cart.js
@@ -82,6 +82,37 @@ export default function CartPage() {
     const price = products.find((p) => p._id === productId)?.price || 0;
     total += parseInt(price);
   }
+
+  async function goToPayment() {
+    const response = await axios.post("/api/checkout", {
+      name,
+      email,
+      city,
+      postalCode,
+      streetAddress,
+      country,
+      cartProducts,
+    });
+    if (response.data.url) {
+      window.location = response.data.url;
+    }
+  }
+
+  if (window.location.href.includes("success")) {
+    return (
+      <>
+        <Header />
+        <Center>
+          <ColumnWrapper>
+            <Box>
+              <h1>Thanks for your order!</h1>
+              <p>We will email you when your order will be sent.</p>
+            </Box>
+          </ColumnWrapper>
+        </Center>
+      </>
+    );
+  }
   return (
     <>
       <Header />
@@ -140,67 +171,65 @@ export default function CartPage() {
           {!!products.length && (
             <Box>
               <h2>Order information</h2>
-              <form method="post" action="/api/checkout">
+              <Input
+                type="text"
+                placeholder="Name"
+                value={name}
+                name="name"
+                onChange={(ev) => {
+                  setName(ev.target.value);
+                }}
+              />
+              <Input
+                type="text"
+                placeholder="Email"
+                value={email}
+                name="email"
+                onChange={(ev) => {
+                  setEmail(ev.target.value);
+                }}
+              />
+              <CityHolder>
                 <Input
                   type="text"
-                  placeholder="Name"
-                  value={name}
-                  name="name"
+                  placeholder="City"
+                  value={city}
+                  name="city"
                   onChange={(ev) => {
-                    setName(ev.target.value);
+                    setCity(ev.target.value);
                   }}
                 />
                 <Input
                   type="text"
-                  placeholder="Email"
-                  value={email}
-                  name="email"
+                  placeholder="Postal Code"
+                  value={postalCode}
+                  name="postalCode"
                   onChange={(ev) => {
-                    setEmail(ev.target.value);
+                    setPostalCode(ev.target.value);
                   }}
                 />
-                <CityHolder>
-                  <Input
-                    type="text"
-                    placeholder="City"
-                    value={city}
-                    name="city"
-                    onChange={(ev) => {
-                      setCity(ev.target.value);
-                    }}
-                  />
-                  <Input
-                    type="text"
-                    placeholder="Postal Code"
-                    value={postalCode}
-                    name="postalCode"
-                    onChange={(ev) => {
-                      setPostalCode(ev.target.value);
-                    }}
-                  />
-                </CityHolder>
-                <Input
-                  type="text"
-                  placeholder="Street Address"
-                  value={streetAddress}
-                  name="streetAddress"
-                  onChange={(ev) => {
-                    setStreetAddress(ev.target.value);
-                  }}
-                />
-                <Input
-                  type="text"
-                  placeholder="Country"
-                  name="country"
-                  value={country}
-                  onChange={(ev) => {
-                    setCountry(ev.target.value);
-                  }}
-                />
-                <Button block black type="submit">
-                  Continue to payment
-                </Button>
-              </form>
+              </CityHolder>
+              <Input
+                type="text"
+                placeholder="Street Address"
+                value={streetAddress}
+                name="streetAddress"
+                onChange={(ev) => {
+                  setStreetAddress(ev.target.value);
+                }}
+              />
+              <Input
+                type="text"
+                placeholder="Country"
+                name="country"
+                value={country}
+                onChange={(ev) => {
+                  setCountry(ev.target.value);
+                }}
+              />
+              <Button block black onClick={goToPayment}>
+                Continue to payment
+              </Button>
             </Box>
           )}
         </ColumnWrapper>
